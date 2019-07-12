0700f1835aadee11424807caa303654c50887dd9
Madison Davis <madisond2302@gmail.com>
2019-07-12 13:25:00 -0500

    Created Two Files

--- Dev Team Doodle/SceneTwo.swift
+++ Dev Team Doodle/SceneTwo.swift
@@ -1,15 +1,16 @@
 //
-//  GameScene.swift
+//  SceneTwo.swift
 //  Dev Team Doodle
 //
 //  Created by Madison Davis on 7/12/19.
 //  Copyright © 2019 Madison Davis. All rights reserved.
 //
 
+import Foundation
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class SceneTwo: SKScene {
     
     override func didMove(to view: SKView) {
         
