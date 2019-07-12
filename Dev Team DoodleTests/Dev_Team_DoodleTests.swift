8a01252b2734915d9757fe9339c9f90e64478f31
Madison Davis <madisond2302@gmail.com>
2019-07-12 13:04:53 -0500

    Initial Commit

---
+++ Dev Team DoodleTests/Dev_Team_DoodleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dev_Team_DoodleTests.swift
+//  Dev Team DoodleTests
+//
+//  Created by Madison Davis on 7/12/19.
+//  Copyright © 2019 Madison Davis. All rights reserved.
+//
+
+import XCTest
+@testable import Dev_Team_Doodle
+
+class Dev_Team_DoodleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
