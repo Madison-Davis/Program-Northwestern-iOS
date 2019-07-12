3ffd701ce3fe1b4ce364715cb5bd2b8b3326ae00
Alexander Yu <yuha@baylorschool.org>
2019-07-12 14:41:34 -0500

    added character physics (untested yet)

--- Dev Team Doodle/SceneTwo.swift
+++ Dev Team Doodle/SceneTwo.swift
@@ -13,12 +13,15 @@ import GameplayKit
 class SceneTwo: SKScene, SKPhysicsContactDelegate {
     
     var brick = SKSpriteNode()
+    var character = SKShapeNode()
     
     override func didMove(to view: SKView) {
         createBackground()
         makeBaseBrick()
+        character.physicsBody?.isDynamic = true
+        character.physicsBody?.applyImpulse(CGVector(dx: Int.random(in: -3...3), dy: 5))
     }
-    
+
     func createBackground() {
         let stars = SKTexture(imageNamed: "stars")
         let starsBackground = SKSpriteNode(texture: stars)
@@ -36,5 +39,31 @@ class SceneTwo: SKScene, SKPhysicsContactDelegate {
             brick.physicsBody?.isDynamic = false
             addChild(brick)
         }
+    }
+    
+    func makeCharacter() {
+        character = SKShapeNode(circleOfRadius: 10)
+        character.position = CGPoint(x: frame.midX, y: frame.midY)
+        character.strokeColor = UIColor.black
+        character.fillColor = UIColor.yellow
+        character.name = "character"
+        
+        // physics shape matches ball image
+        character.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        // ignores all forces and impulses
+        character.physicsBody?.isDynamic = false
+        // use precise collision detection
+        character.physicsBody?.usesPreciseCollisionDetection = true
+        // no loss of energy from friction
+        character.physicsBody?.friction = 0
+        // gravity is not a factor
+        character.physicsBody?.affectedByGravity = true
+        // bounces fully off of other objects
+        character.physicsBody?.restitution = 1
+        // does not slow down over time
+        character.physicsBody?.linearDamping = 0
+        character.physicsBody?.contactTestBitMask = (character.physicsBody?.collisionBitMask)!
+        
+        addChild(character) // add ball object to the view
     }
 }
